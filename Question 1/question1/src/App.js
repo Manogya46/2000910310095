98ff236020f645ba253b70c2a29664642db8e8ef
Manogya46 <mishramanogya46@gmail.com>
2023-07-22 10:56:12 +0530

    first commit

---
+++ Question 1/question1/src/App.js
@@ -0,0 +1,6 @@
+
+function App() {
+  return <h1>Develop number management HTTP microservice</h1>
+}
+
+export default App;
---
+++ Question 2/question2/src/App.js
@@ -0,0 +1,7 @@
+
+
+function App() {
+  return <h1>Develop a react based frontend web app to display train schdule</h1>
+}
+
+export default App;
